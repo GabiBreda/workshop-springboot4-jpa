3069207504cb86ed8430428c06905ab8f2b984fd
Gabriel Breda <gabibreda2015@gmail.com>
2026-03-28 10:46:22 -0300

    `Many-to-many` association with JoinTable

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -35,11 +35,6 @@ public class TestConfig implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        User u1 = new User(null, "Maria Brown","maria@gmail.com","88888888","123456");
-        User u2 = new User(null, "Alex Green","alex@gmail.com","77777777","123456");
-
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID,u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-20T20:07:56Z"), OrderStatus.DELIVERED,u2);
 
         Category cat1 = new Category(null, "Electronics");
         Category cat2 = new Category(null, "Books");
@@ -47,9 +42,22 @@ public class TestConfig implements CommandLineRunner {
         Product p1 = new Product(null, "Clock", "Texto generico", 90.5, "");
         Product p2 = new Product(null, "Tv", "Texto generico", 2290.5, "");
 
-        userRepository.saveAll(Arrays.asList(u1,u2));
-        orderRepository.saveAll(Arrays.asList(o1,o2));
         categoryRepository.saveAll(Arrays.asList(cat1,cat2));
         productRepository.saveAll(Arrays.asList(p1,p2));
+
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+
+        productRepository.saveAll(Arrays.asList(p1,p2));
+
+        User u1 = new User(null, "Maria Brown","maria@gmail.com","88888888","123456");
+        User u2 = new User(null, "Alex Green","alex@gmail.com","77777777","123456");
+
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID,u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-20T20:07:56Z"), OrderStatus.DELIVERED,u2);
+
+        userRepository.saveAll(Arrays.asList(u1,u2));
+        orderRepository.saveAll(Arrays.asList(o1,o2));
+
     }
 }
